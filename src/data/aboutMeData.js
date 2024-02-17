41fe5b0b3c29bc475fb3f8a87119e462494b3766
mido <midosaedsef@gmail.com>
2024-02-17 16:26:02 +0200

    ssss

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,10 +1,19 @@
 export const aboutMeData = [
-	{
-		id: 1,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-	},
-	{
-		id: 2,
-		bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-	},
+  {
+    id: 1,
+    bio: `Hey there! 👋 I'm Mohamed Saed Sef, a frontend developer with a knack for crafting delightful user experiences. Based in  Egypt , Asswan , I'm passionate about leveraging cutting-edge technologies to build intuitive and visually appealing web applications.`,
+  },
+  {
+    id: 2,
+    bio: `What I Do\n
+	As a frontend developer, I specialize in bringing designs to life and creating seamless interactions that engage users. From translating mockups into responsive layouts to optimizing performance and accessibility, I thrive on the challenge of turning ideas into polished web experiences.`,
+  },
+  {
+    id: 3,
+    bio: `Here are some of my skills and technologies:`,
+  },
+  {
+    id: 4,
+    bio: ` HTML, CSS, JavaScript, Typescript, Tailwind, React, Figma, Git, and GitHub.`,
+  },
 ];
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,51 +1,52 @@
 // Import images
-import WebImage1 from '../images/web-project-1.jpg';
-import WebImage2 from '../images/web-project-2.jpg';
-import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
-import UIImage1 from '../images/ui-project-1.jpg';
-import UIImage2 from '../images/ui-project-2.jpg';
+import WebImage1 from "../images/web-project-1.jpg";
+import LMS from "../images/LMS/LMS.png";
+import Dot from "../images/Dot/Dot.png";
+import MobileImage1 from "../images/mobile-project-1.jpg";
+import MobileImage2 from "../images/mobile-project-2.jpg";
+import Todo from "../images/Todo/Todo.png";
+import UIImage2 from "../images/ui-project-2.jpg";
 
 export const projectsData = [
-	{
-		id: 1,
-		title: 'Google Health Platform',
-		category: 'Web Application',
-		img: WebImage2,
-		ProjectHeader: {
-			title: 'Project Management UI - From Context',
-			publishDate: 'Jul 26, 2021',
-			tags: 'UI / Frontend',
-		},
-	},
-	{
-		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Mobile Application',
-		img: MobileImage2,
-	},
-	{
-		id: 3,
-		title: 'Project Management UI',
-		category: 'UI/UX Design',
-		img: UIImage1,
-	},
-	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
-		img: UIImage2,
-	},
-	{
-		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
-		img: MobileImage1,
-	},
-	{
-		id: 6,
-		title: 'Apple Design System',
-		category: 'Web Application',
-		img: WebImage1,
-	},
+  {
+    id: 1,
+    title: "LMS system for schools",
+    category: "Web Application",
+    img: LMS,
+    ProjectHeader: {
+      title: "Project Management UI - From Context",
+      publishDate: "Jul 26, 2021",
+      tags: "UI / Frontend",
+    },
+  },
+  {
+    id: 2,
+    title: ".Dot Restaurant App",
+    category: "Web Application",
+    img: Dot,
+  },
+  {
+    id: 3,
+    title: "Todo List App",
+    category: "Web Application",
+    img: Todo,
+  },
+  {
+    id: 4,
+    title: "Cloud Storage Platform",
+    category: "UI/UX Design",
+    img: UIImage2,
+  },
+  {
+    id: 5,
+    title: "React Social App",
+    category: "Mobile Application",
+    img: MobileImage1,
+  },
+  {
+    id: 6,
+    title: "Apple Design System",
+    category: "Web Application",
+    img: WebImage1,
+  },
 ];
